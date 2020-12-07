a8bb167e1e4298ac49ed3111fe035a5b42966c13
Andreas Thörn <andreas.thorn908@gmail.com>
2020-12-07 22:54:31 +0100

    Solution day 7

--- src/main/java/Day07.java
+++ src/main/java/Day07.java
@@ -1,5 +1,8 @@
+import org.javatuples.Pair;
+
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day07 {
     private static List<String> readInput(InputStream stream) {
@@ -10,54 +13,70 @@ public class Day07 {
         return result;
     }
 
-    private static String SHINY_GOLD_BAG = "shiny gold";
-    private static String EMPTY_BAG = "no other";
-    private static HashMap<String, HashMap<String, Integer>> bags = new HashMap<>();
+    private static final String SHINY_GOLD_BAG = "shiny gold";
+    private static final String EMPTY_BAG = "no other";
+    private static HashMap<String, HashMap<String, Integer>> bags;
 
-    private static void parseBagRules(final LinkedList<String> rules) {
+    private static Pair<String, List<String>> formatRuleText(String ruleText) {
+        String strippedRule = ruleText.replaceAll("bags|bag", "").replace(" .", "");
+        String[] ruleSplit = strippedRule.split(" contain ");
+
+        String bagName = ruleSplit[0].strip();
+        List<String> payload = Arrays.stream(ruleSplit[1].split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        return new Pair<>(bagName, payload);
+    }
+
+    private static HashMap<String, HashMap<String, Integer>> parseRules(final LinkedList<String> rules) {
+        HashMap<String, HashMap<String, Integer>> result = new HashMap<>();
+
         for(String bagRule : rules) {
-            String strippedRule = bagRule.replaceAll("bags|bag", "").replace(" .", "");
-            String[] res = strippedRule.split(" contain ");
+            var formattedBagRule = formatRuleText(bagRule);
 
             HashMap<String, Integer> bagPayload = new HashMap<>();
-            for(String payload : Arrays.asList(res[1].split(","))) {
+            for(String payload : formattedBagRule.getValue1()) {
                 if (payload.equals(EMPTY_BAG)) break;
 
-                payload = payload.strip();
                 String[] payloadSplit = payload.split(" ", 2);
                 bagPayload.put(payloadSplit[1], Integer.parseInt(payloadSplit[0]));
             }
-            bags.put(res[0].strip(), bagPayload);
+            result.put(formattedBagRule.getValue0(), bagPayload);
         }
+        return result;
     }
 
     private static boolean canContainGoldenBag(String bag) {
-        if(!bags.containsKey(bag)) return false;
-        if(bags.get(bag).keySet().stream().anyMatch(bag1 -> bag1.contains(SHINY_GOLD_BAG))) return true;
+        if(bags.get(bag).keySet().stream().anyMatch(b -> b.contains(SHINY_GOLD_BAG)))
+            return true;
         return bags.get(bag).keySet().stream().anyMatch(Day07::canContainGoldenBag);
     }
 
     private static int firstTask() {
-        return (int) bags.keySet().stream().filter(Day07::canContainGoldenBag).count();
+        return (int) bags.keySet().stream()
+                .filter(Day07::canContainGoldenBag)
+                .count();
     }
 
-    private static int totalBags(String bag) {
+    private static int bagsRequired(String bag) {
         int result = 1;
-        for (Map.Entry<String,Integer> entry : bags.get(bag).entrySet())
-            result += (totalBags(entry.getKey()) * entry.getValue());
+        var bagPayload = bags.get(bag);
+        for (Map.Entry<String,Integer> entry : bagPayload.entrySet())
+            result += (bagsRequired(entry.getKey()) * entry.getValue());
 
         return result;
     }
 
     private static long secondTask() {
-        return totalBags(SHINY_GOLD_BAG) - 1; // Subtract shiny gold bag.
+        return bagsRequired(SHINY_GOLD_BAG) - 1; // Subtract shiny gold bag.
     }
 
     public static void main(String[] args) {
         LinkedList<String> input = new LinkedList<>(
                 readInput(Day07.class.getClassLoader().getResourceAsStream("day_07_input.txt")));
 
-        parseBagRules(input);
+        bags = parseRules(input);
         System.out.printf("First task: %d%n", firstTask());
         System.out.printf("Second task: %d%n", secondTask());
     }
